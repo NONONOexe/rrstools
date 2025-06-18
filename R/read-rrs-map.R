36f3294ff40a3c38aac6b9034762d4299ab4dca6
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-06-19 08:02:12 +0900

    :art: Change call function format !#1

--- R/read-rrs-map.R
+++ R/read-rrs-map.R
@@ -18,8 +18,8 @@ NULL
 
 # Parse nodes from the XML and return a sf object
 parse_nodes <- function(xml, scale_data) {
-  nodes <- xml_find_all(xml, ".//gml:Node")
-  coordinates <- strsplit(xml_text(nodes), ",")
+  nodes <- xml2::xml_find_all(xml, ".//gml:Node")
+  coordinates <- strsplit(xml2::xml_text(nodes), ",")
 
   x <- as.numeric(sapply(coordinates, "[[", 1))
   y <- as.numeric(sapply(coordinates, "[[", 2))
@@ -30,17 +30,17 @@ parse_nodes <- function(xml, scale_data) {
     y <- y - min(y)
   }
 
-  return(st_as_sf(
-    data.frame(id = xml_attr(nodes, "id"), x, y),
+  return(sf::st_as_sf(
+    data.frame(id = xml2::xml_attr(nodes, "id"), x, y),
     coords = c("x", "y"), crs = NA, agr = "constant"
   ))
 }
 
 # Extract node references and orientations from edges
 extract_edge_nodes <- function(edges) {
-  directed_nodes <- xml_find_all(edges, ".//gml:directedNode")
-  node_ref <- sub("^#", "", xml_attr(directed_nodes, "href"))
-  orientations <- xml_attr(directed_nodes, "orientation")
+  directed_nodes <- xml2::xml_find_all(edges, ".//gml:directedNode")
+  node_ref <- sub("^#", "", xml2::xml_attr(directed_nodes, "href"))
+  orientations <- xml2::xml_attr(directed_nodes, "orientation")
 
   return(list(
     source = node_ref[orientations == "+"],
@@ -50,29 +50,29 @@ extract_edge_nodes <- function(edges) {
 
 # Parse edges and create a sf object
 parse_edges <- function(xml, node_sf) {
-  edges <- xml_find_all(xml, ".//gml:Edge")
+  edges <- xml2::xml_find_all(xml, ".//gml:Edge")
   edge_nodes <- extract_edge_nodes(edges)
 
   source_nodes <- node_sf$geometry[match(edge_nodes$source, node_sf$id)]
   target_nodes <- node_sf$geometry[match(edge_nodes$target, node_sf$id)]
 
   geometries <- mapply(
-    function(p1, p2) st_linestring(c(p1, p2)),
+    function(p1, p2) sf::st_linestring(c(p1, p2)),
     source_nodes, target_nodes, SIMPLIFY = FALSE
   )
 
-  return(st_sf(
-    id = xml_attr(edges, "id"),
+  return(sf::st_sf(
+    id = xml2::xml_attr(edges, "id"),
     geometry = geometries, crs = NA, agr = "constant"
   ))
 }
 
 # Group and combine edges for buildings or roads
 group_edges_by_face <- function(edges, faces) {
-  direct_edges <- xml_find_all(faces, ".//gml:directedEdge")
-  edge_ids <- sub("^#", "", xml_attr(direct_edges, "href"))
+  direct_edges <- xml2::xml_find_all(faces, ".//gml:directedEdge")
+  edge_ids <- sub("^#", "", xml2::xml_attr(direct_edges, "href"))
   matched_edges <- edges$geometry[match(edge_ids, edges$id)]
-  group_indices <- rep(seq_along(faces), xml_length(faces))
+  group_indices <- rep(seq_along(faces), xml2::xml_length(faces))
 
   return(split(matched_edges, group_indices))
 }
@@ -80,17 +80,17 @@ group_edges_by_face <- function(edges, faces) {
 # Parse faces (building or roads) and create a sf object
 parse_faces <- function(xml, edge_sf, type = c("building", "road")) {
   type <- match.arg(type)
-  features <- xml_find_all(xml, paste0(".//rcr:", type))
-  faces <- xml_find_all(features, ".//gml:Face")
+  features <- xml2::xml_find_all(xml, paste0(".//rcr:", type))
+  faces <- xml2::xml_find_all(features, ".//gml:Face")
   edge_groups <- group_edges_by_face(edge_sf, faces)
 
   geometries <- sapply(edge_groups, function(lines) {
-    combined_lines <- st_union(lines)
-    st_collection_extract(st_polygonize(combined_lines), "POLYGON")
+    combined_lines <- sf::st_union(lines)
+    sf::st_collection_extract(sf::st_polygonize(combined_lines), "POLYGON")
   })
 
-  return(st_sf(
-    id = xml_attr(features, "id"),
+  return(sf::st_sf(
+    id = xml2::xml_attr(features, "id"),
     geometry = geometries, crs = NA, agr = "constant"
   ))
 }
@@ -99,7 +99,7 @@ parse_faces <- function(xml, edge_sf, type = c("building", "road")) {
 #' @rdname read_rrs_map
 #' @export
 read_rrs_map <- function(gml, scale_data = FALSE) {
-  xml <- read_xml(gml)
+  xml <- xml2::read_xml(gml)
 
   node_sf <- parse_nodes(xml, scale_data)
   edge_sf <- parse_edges(xml, node_sf)
