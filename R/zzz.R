ae2790066d9cc3e94da5f96a9f2c70a3f1c46afa
Keisuke ANDO <g.kei0429@gmail.com>
2026-06-02 23:44:07 +0900

    Revert "rrstools 1.1.0"
    This reverts commit af2b67ff6420873e44356b79d18545971c1f4ecc.

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.rrstools_env <- new.env(parent = emptyenv())
-
-.onLoad <- function(libname, pkgname) {
-  .rrstools_env$hub_url <- "https://raw.githubusercontent.com/nononoexe/rcrs-scenario-hub/main/scenarios.json"
-  .rrstools_env$scenarios_cache <- NULL
-}
